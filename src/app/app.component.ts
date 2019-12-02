067cb42a0525b9747e8a4d64bb98a422c661d1a4
Naresh Kumar Yeduvaka <yushanaresh098@gmail.com>
2019-12-02 22:22:09 +0530

    data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kumar';
+  val = parseInt((<HTMLInputElement>document.getElementById("naresh")).value)
+  msg="";
+  Addition(){
+    this.msg=""
+    this.val+=1
+  }
+  Subtract(){
+    if(this.val==0){
+      this.msg="value reached to 0";
+    }
+    else{
+    this.val-=1;
+  }
+}
 }
